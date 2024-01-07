cc3798f1f7cf6923dd4620288d96755c6c2eb399
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-01-07 17:40:08 +1100

    Completed application, added comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,78 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Variables that needed to be declared before the page loaded.
 var currentHour;
-var userText = ["", "", "", "", "", "", "", "",];
-var saveButtons = $('.saveBtn');
+var userText = ['', '', '', '', '', '', '', '', ''];
+
 
 $(function () {
-  for(i = 9; i < 17; i++) {
-    console.log($('#hour-' + i));
-    $('#hour-' + i).children('button').on('click', function() {
-      userText[0] = $('#hour-' + i).children('textarea').val();
-      console.log($('#hour-' + i).children('textarea').val());
-    });
+  // Variables that needed to be declared after the page has loaded.
+  var storedUserText = JSON.parse(localStorage.getItem('storedUserText'))
+
+  // Checks to see if the user has previously stored Work Day information
+  if (storedUserText !== null) {
+    // This for loop is used to dynamically create global variables for the div ids in HTML as well as set the text of the page to what the user previously had stored.
+    for(i = 9; i <= 17; i++) {
+      window['hour' + i] = $('#hour-' + i);
+      $('#hour-' + i).children('textarea').text(storedUserText[i - 9]);
+    }
+    // Sets the userText variable to what was stored in the users browser.
+    userText = storedUserText;
+    // If the user has no stored data the application keeps user text as blank and only creates the variables.
+  } else {
+    for(i = 9; i <= 17; i++) {
+      window['hour' + i] = $('#hour-' + i);
+    }
   }
 
-  // console.log(saveBtn1);
-
-
+  // Clock function to display and keep track of current user system time.
   function clock() {
     headerClock = setInterval(function () {
-      var currentTime = dayjs().format('MMM D, hh:mm:ss a');
+      // Uses DayJS to create a variable that keeps track of the users current system time.
+      var currentTime = dayjs().format('dddd, MMMM D, h:mm:ss a');
+      // Displays the currentTime variable on this webpage.
       $('#currentDay').text(currentTime);
+      
+      // Creates a currentHour var using dayjs for conversion to 24 hour time.
       currentHour = dayjs().format('hh');
-      // Since this project only uses 9am-5pm I can create a psuedo '24h clock'
-      // that is only relevant to the projects time parameters, if I needed to
-      // make an actual 24h clock I would just check to see if a seperate dayjs() 
-      // variable was AM or PM and apply the same logic to manipulate the 'currentHour' that way,
-      // if I did it this way I would also need to add logic to check if it was 12am and subtract 12
-      // creating a true 24h clock.
-      if (currentHour < 8) {
+      // This var checks dispays whether it is am or pm.
+      check12h = dayjs().format('a');
+
+      // Converts currentHour to 24hour time
+      // Checks to see if it is past noon AND not 12 (if it is 12 there is no need to add anything) if true then adds 12.
+      if (check12h == 'pm' && currentHour !== 12) {
         currentHour = parseInt(currentHour) + 12;
+      // If the conditions are not true then simply converts currentHour into an integer.
+      } else {
+        currentHour = parseInt(currentHour);
       }
-      console.log(currentHour);
-      curHourDiv = $('#hour-' + currentHour);
-      return currentHour, curHourDiv;
-  
+
+      // Checks to see if it is midnight, if true sets currentHour to 00, staying true to 24h time.
+      if (check12h == 'am' && currentHour == 12) {
+        currentHour = currentHour - 12;
+      }
+      
+      // Uses a for loop to assign class past to all hours prior in the day and ensure that present class is unassigned to the prior hour.
+      for (i = 9; i < currentHour; i++) {
+        $('#hour-' + i).addClass('past');
+        $('#hour-' + i).removeClass('present');
+      }
+
+      // Assigns class present to the div of the current hour.
+      $('#hour-' + currentHour).addClass('present');
+      $('#hour-' + currentHour).removeClass('future');
+
+      // Uses a for loop to assign class future to all hour divs greater than current hour.
+      for (i = currentHour + 1; i <= 17; i++) {
+        $('#hour-' + i).addClass('future');
+        // And removes any of the other timeframe classes.
+        $('#hour-' + i).addClass('present');
+        $('#hour-' + i).addClass('past');
+      }
+
+    // Updates every second.
     }, 1000);
   }
   
-
+  // Calls clock function.
   clock();
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -47,27 +81,54 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
+  // The following functions create event listeners for each hour button.
+  hour9.children('button').on('click', function() {
+    // Checks what the user has input in the text area of the click event div.
+    userText[0] = hour9.children('textarea').val();
+    // Logs updated userText
+    console.log(userText);
+    // Updates the storedUserText to the users new input, converting the array into a string as it does so.
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour10.children('button').on('click', function() {
+    userText[1] = hour10.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour11.children('button').on('click', function() {
+    userText[2] = hour11.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour12.children('button').on('click', function() {
+    userText[3] = hour12.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour13.children('button').on('click', function() {
+    userText[4] = hour13.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour14.children('button').on('click', function() {
+    userText[5] = hour14.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour15.children('button').on('click', function() {
+    userText[6] = hour15.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem("storedUserText", JSON.stringify(userText));
+  });
+  hour16.children('button').on('click', function() {
+    userText[7] = hour16.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
+  hour17.children('button').on('click', function() {
+    userText[8] = hour17.children('textarea').val();
+    console.log(userText);
+    localStorage.setItem('storedUserText', JSON.stringify(userText));
+  });
 });
-
-
-
-
-/* function handleSaveToLocal(event) {
-  userText = saveBtn.parent().children('textarea').text();
-  console.log(userText + "test");
-}
-
-saveBtn.addEventListener('click', handleSaveToLocal);
-*/
